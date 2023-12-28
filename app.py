c8badb6610be806c22bdc29e19b4d44cf50eaf83
dudunwei <ddw99402@qq.com>
2023-12-28 21:27:45 +0800

    更改

--- app.py
+++ app.py
@@ -67,10 +67,11 @@ def euclidean_distance(x1, x2):
 # 第一个功能的路由处理函数
 @app.route('/stat/closest_cities', methods=['GET'])
 def closest_cities():
-    city_name = request.args.get('city')
+    city_name = request.args.get('city').strip().lower()  # 将输入转换为小写并去除空格
+    state_name = request.args.get('state').strip().lower()  # 同上
     page_size = int(request.args.get('page_size', 50))
-    page = int(request.args.get('page', 0))
-    cache_key = f"closest_cities:{city_name}:{page_size}:{page}"
+    page = int(request.args.get('page', 1))
+    cache_key = f"closest_cities:{city_name}:{state_name}:{page_size}:{page}"
 
     start_time = time.time()
     # 尝试从 Redis 获取缓存的结果
@@ -80,8 +81,8 @@ def closest_cities():
         return jsonify({"result": json.loads(cached_result), "from_cache": True, "compute_time": elapsed_time})
 
     # 从数据库获取指定城市的坐标
-    query = "SELECT c.lat, c.lng FROM c WHERE c.city = @city"
-    parameters = [{"name": "@city", "value": city_name}]
+    query = "SELECT c.lat, c.lng FROM c WHERE LOWER(c.city) = @city AND LOWER(c.state) = @state"
+    parameters = [{"name": "@city", "value": city_name}, {"name": "@state", "value": state_name}]
     city_data = list(container_cities.query_items(query=query, parameters=parameters, enable_cross_partition_query=True))
 
     if not city_data:
@@ -89,23 +90,30 @@ def closest_cities():
 
     city_lat, city_lng = city_data[0]['lat'], city_data[0]['lng']
 
-    # 获取所有城市并计算欧拉距离
+    # 获取所有城市并计算距离
     cities = list(container_cities.read_all_items())
+    cities_with_distance = []
     for city in cities:
-        city['distance'] = calculate_eular_distance(city_lat, city_lng, city['lat'], city['lng'])
+        city_name_lower = city['city'].lower()
+        state_name_lower = city['state'].lower()
+        if city_name_lower != city_name or state_name_lower != state_name:  # 排除查询的城市
+            city['distance'] = calculate_eular_distance(city_lat, city_lng, city['lat'], city['lng'])
+            cities_with_distance.append(city)
 
     # 根据距离排序并实现分页
-    cities.sort(key=lambda x: x['distance'])
-    start = page * page_size
+    cities_with_distance.sort(key=lambda x: x['distance'])
+    start = (page - 1) * page_size
     end = start + page_size
-    paginated_cities = cities[start:end]
+    paginated_cities = cities_with_distance[start:end]
 
     # 将结果缓存到 Redis
-    cache.set(cache_key, json.dumps(paginated_cities), ex=60*60)  # 缓存一个小时
+    cache.set(cache_key, json.dumps(paginated_cities), ex=60*60)
 
     elapsed_time = int((time.time() - start_time) * 1000)
 
     return jsonify({"result": paginated_cities, "from_cache": False, "compute_time": elapsed_time})
+
+
 
 
 
@@ -208,10 +216,144 @@ def knn_reviews():
     except Exception as e:
         return jsonify({"error": str(e)})
 
-@app.route('/')
+
+# 计算城市的平均评论评分
+@app.route('/stat/city_reviews', methods=['GET'])
+def city_reviews():
+    city_name = request.args.get('city').strip().lower()
+    state_name = request.args.get('state').strip().lower()
+    page_size = int(request.args.get('page_size', 10))
+    page = int(request.args.get('page', 1))
+    cache_key = f"city_reviews:{city_name}:{state_name}:{page_size}:{page}"
+
+    start_time = time.time()
+    cached_result = cache.get(cache_key)
+    if cached_result:
+        elapsed_time = int((time.time() - start_time) * 1000)
+        return jsonify({"result": json.loads(cached_result), "from_cache": True, "compute_time": elapsed_time})
+
+    # 从数据库获取指定城市的坐标
+    query = "SELECT c.lat, c.lng FROM c WHERE LOWER(c.city) = @city AND LOWER(c.state) = @state"
+    parameters = [{"name": "@city", "value": city_name}, {"name": "@state", "value": state_name}]
+    city_data = list(container_cities.query_items(query=query, parameters=parameters, enable_cross_partition_query=True))
+
+    if not city_data:
+        return jsonify({"error": "City not found"}), 404
+
+    city_lat, city_lng = city_data[0]['lat'], city_data[0]['lng']
+
+    # 从数据库获取所有评论并计算每个城市的平均评分
+    all_reviews = list(container_reviews.read_all_items())
+    city_scores = {}
+    for review in all_reviews:
+        city_lower = review['city'].lower()
+        if city_lower not in city_scores:
+            city_scores[city_lower] = {'total_score': 0, 'count': 0}
+
+        # 确保评分是整数类型
+        try:
+            score = int(review['score'])
+        except ValueError:
+            # 如果评分不能转换为整数，则跳过这条评论
+            continue
+
+        city_scores[city_lower]['total_score'] += score
+        city_scores[city_lower]['count'] += 1
+
+    for key, value in city_scores.items():
+        value['average_score'] = value['total_score'] / value['count']
+
+    # 获取所有城市的位置信息并计算距离
+    all_cities = list(container_cities.read_all_items())
+    cities_with_scores = []
+    for city in all_cities:
+        city_lower = city['city'].lower()
+        if city_lower in city_scores:
+            distance = calculate_eular_distance(city_lat, city_lng, city['lat'], city['lng'])
+            cities_with_scores.append({
+                'city': city['city'],
+                'state': city['state'],
+                'average_score': city_scores[city_lower]['average_score'],
+                'distance': distance
+            })
+
+    cities_with_scores.sort(key=lambda x: x['distance'])
+
+    # 分页
+    start = (page - 1) * page_size
+    end = start + page_size
+    paginated_cities = cities_with_scores[start:end]
+
+    cache.set(cache_key, json.dumps(paginated_cities), ex=60*60*24*5)
+    elapsed_time = int((time.time() - start_time) * 1000)
+
+    return jsonify({"result": paginated_cities, "from_cache": False, "compute_time": elapsed_time})
+
+
+@app.route('/stat/clustering_pie_chart', methods=['GET'])
+def clustering_pie_chart():
+    try:
+        classes = int(request.args.get('classes', 6))
+        words = int(request.args.get('words', 100))
+
+        # 构建缓存键
+        cache_key = f'clustering_pie_chart:{classes}:{words}'
+
+        # 尝试从 Redis 获取缓存的结果
+        cached_result = cache.get(cache_key)
+        if cached_result:
+            return jsonify(json.loads(cached_result))
+
+        # 从 Cosmos DB 读取评论数据
+        reviews_data = []
+        query = "SELECT c.review FROM c"
+        reviews = container_reviews.query_items(query=query, enable_cross_partition_query=True)
+        for review in reviews:
+            reviews_data.append(review['review'])
+
+        # 使用 TF-IDF 对评论文本进行向量化
+        vectorizer = TfidfVectorizer(max_features=words, stop_words='english')
+        X = vectorizer.fit_transform(reviews_data)
+
+        # 使用 KMeans 算法进行聚类
+        kmeans = KMeans(n_clusters=classes)
+        kmeans.fit(X)
+
+        # 计算每个聚类的样本数量
+        class_counts = np.bincount(kmeans.labels_, minlength=classes)
+
+        result = {
+            "class_counts": class_counts.tolist(),
+            "classes": classes
+        }
+
+        # 缓存结果到 Redis
+        cache.set(cache_key, json.dumps(result), ex=3600*24*5)  # 缓存5天
+
+        return jsonify({
+            "class_counts": class_counts.tolist(),
+            "classes": classes
+        })
+    except Exception as e:
+        return jsonify({"error": str(e)})
+
+
+@app.route('/q10')
+def query_cities():
+    return render_template('index3.html')
+
+
+@app.route('/q11')
+def review_cities():
+    return render_template('index4.html')
+
+
+@app.route('/q12')
 def index():
-    return render_template('index2.html')
+    return render_template('index.html')
 # Dunwei Du 76004 & Qingwei Zeng 76028
+
+
 if __name__ == '__main__':
     app.run(debug=True)
 
